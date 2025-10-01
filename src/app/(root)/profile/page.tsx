556ef4c3178f9d15c45b9b96466d23ff9b56aabd
Vivek <kumarvivek83561@gmail.com>
2025-10-01 23:32:31 +0530

    feat: update Avatar and CartItem components for improved responsiveness and styling

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -12,7 +12,7 @@ export default function page() {
             <div>
                 <div className='flex gap-6 items-center pb-20'>
 
-                    <Avatar className='w-30 h-30 flex items-center justify-center object-center object-cover overflow-hidden rounded-full cursor-pointer'>
+                    <Avatar className='w-15 h-15 sm:w-20 sm:h-20 md:w-30 md:h-30 flex items-center justify-center object-center object-cover overflow-hidden rounded-full cursor-pointer'>
                         <AvatarImage
                             className='w-full h-full'
                             src=""
@@ -21,16 +21,16 @@ export default function page() {
                         <AvatarFallback className=" w-full h-full flex items-center justify-center bg-dark-700 text-white"></AvatarFallback>
                     </Avatar>
 
-                    <div className='flex flex-col gap-2'>
-                        <h2 className='text-heading-3'>Vivek kumar</h2>
-                        <span>vivek@gmail.com</span>
+                    <div className='flex flex-col gap-1 md:gap-2'>
+                        <h2 className='sm:text-sm md:text-md  md:text-heading-3 font-semibold'>Vivek kumar</h2>
+                        <span className='sm:text-sm md:text-md '>vivek@gmail.com</span>
                     </div>
 
                 </div>
-                <ul className='flex gap-12 mb-10'>
+                <ul className='flex gap-4 flex-wrap md:gap-12 mb-10'>
                     {
                         profileOptions.map((option,idx)=>(
-                            <span key={idx} className={`${option === selectedOption ? 'border-b-2 border-dark-700':''} pb-1 transition-transform duration-300 cursor-pointer`} onClick={()=>setSelectedOption(option)}>{option}</span>
+                            <span key={idx} className={`${option === selectedOption ? 'border-b-2 border-dark-700':''} pb-1  cursor-pointer text-sm md:text:md lg:text-lg`} onClick={()=>setSelectedOption(option)}>{option}</span>
                         ))
                     }
                 </ul>
