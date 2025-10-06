44e814df78dff5fb8ed07719b72a2e2b2bdf184f
Vivek <kumarvivek83561@gmail.com>
2025-10-06 18:55:01 +0530

    feat: implement authentication API routes for GET and POST requests

---
+++ src/app/api/auth/[...all]/route.ts
@@ -0,0 +1,15 @@
+import { toNextJsHandler } from "better-auth/next-js";
+import {NextRequest} from "next/server";
+
+
+export async function GET(req:NextRequest){
+    const { auth } = await import("@/lib/auth")
+    const { GET } = toNextJsHandler(auth)
+    return GET(req);
+}
+
+export async function POST(req: NextRequest) {
+  const { auth } = await import("@/lib/auth");
+  const { POST } = toNextJsHandler(auth);
+  return POST(req);
+}
